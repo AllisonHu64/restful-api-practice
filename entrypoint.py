a32205a53f85e8592fd9218ec38c23edd9bb4c4f
Allison Hu <pan.hu46@gmail.com>
2022-12-21 16:38:31 -0500

    Add UsersApi resource to Flask app in entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,12 @@
+""" Adds api resources to Flask application.
+"""
+from api.config.config import Config
+from api.resources.users_api import UsersApi
 from flask import Flask
+from flask_restful import Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+api.add_resource(UsersApi, "{}users".format(Config.instance().APPLICATION_ROOT))
+api.init_app(app)
